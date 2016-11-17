2374bdc5efe1cf2dfa97742bbf30629bcaa0fce0
Chris Perez <chris@chrisperez.io>
2016-11-16 16:37:57 -0800

    started

---
+++ TwentyQuestions/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TwentyQuestions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.QuestionLabel = new System.Windows.Forms.Label();
+            this.YesButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // QuestionLabel
+            // 
+            this.QuestionLabel.AutoSize = true;
+            this.QuestionLabel.Location = new System.Drawing.Point(69, 46);
+            this.QuestionLabel.Name = "QuestionLabel";
+            this.QuestionLabel.Size = new System.Drawing.Size(70, 25);
+            this.QuestionLabel.TabIndex = 0;
+            this.QuestionLabel.Text = "label1";
+            // 
+            // YesButton
+            // 
+            this.YesButton.Location = new System.Drawing.Point(74, 172);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(189, 66);
+            this.YesButton.TabIndex = 1;
+            this.YesButton.Text = "Yes";
+            this.YesButton.UseVisualStyleBackColor = true;
+            this.YesButton.Click += new System.EventHandler(this.YesButton_Click);
+            // 
+            // NoButton
+            // 
+            this.NoButton.Location = new System.Drawing.Point(331, 172);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(189, 64);
+            this.NoButton.TabIndex = 2;
+            this.NoButton.Text = "No";
+            this.NoButton.UseVisualStyleBackColor = true;
+            this.NoButton.Click += new System.EventHandler(this.NoButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 1270);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.YesButton);
+            this.Controls.Add(this.QuestionLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label QuestionLabel;
+        private System.Windows.Forms.Button YesButton;
+        private System.Windows.Forms.Button NoButton;
+    }
+}
+
---
+++ TwentyQuestions/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwentyQuestions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Question root;
+        Question current;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            root = new Question();
+            root.question = "Is it hot?";
+            root.yes = new Question();
+            root.yes.question = "Is it coffee?";
+            root.no = new Question();
+            root.no.question = "Is it tea?";
+
+            current = root;
+            QuestionLabel.Text = current.question; 
+        }
+
+        private void YesButton_Click(object sender, EventArgs e)
+        {
+            if(current.IsLeaf())
+            {
+                if (MessageBox.Show("I win! Do you want to play again?", "Twenty Q", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    this.DialogResult = DialogResult.Cancel;
+                    this.Close();
+                }
+                else
+                {
+                    //relaunch game?   
+                }
+            }
+            else
+            {
+                current = new Question( )
+            }
+        }
+
+        private void NoButton_Click(object sender, EventArgs e)
+        {
+            if(current.IsLeaf())
+            {
+
+            }
+            //am I at a leaf?
+            //no: ask the next question
+            //yes: new thing... go learn stuff
+            // Do you want to play again?
+
+            AddNewItem add = new AddNewItem();
+            if(add.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+    }
+}
